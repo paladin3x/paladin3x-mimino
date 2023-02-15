2874a6c829b6dea0bd7ffee401032d32070883ae
Roman Babik <kazantip2020rb@gmail.com>
2023-02-15 15:22:43 +0200

    fixed restaurant page

--- src/index.js
+++ src/index.js
@@ -1,20 +1,41 @@
 import './js/modal'
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper1, { Navigation } from 'swiper';
+import Swiper2, { Navigation } from 'swiper';
 
 // import styles bundle
 
 // configure Swiper to use modules
-Swiper.use([Navigation, Pagination]);
+Swiper1.use([Navigation]);
 
 // init Swiper:
-const swiper = new Swiper('.swiper-hero, .swiper-restaurant', {
+const swiper = new Swiper1('.swiper-hero', {
   loop: true,
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next, .every-arrow-prev',
     prevEl: '.swiper-button-prev, .every-arrow-next',
   },
+
+  slidesPerView: 1,
 });
+
+Swiper2.use([Navigation]);
+
+// init Swiper:
+const swiper1 = new Swiper2('.swiper-restaurant', {
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.every-arrow-prev',
+    prevEl: '.every-arrow-next',
+  },
+
+  spaceBetween: 20,
+  slidesPerView: 1.2,
+  speed: 500,
+});
+
+
 
 
 
